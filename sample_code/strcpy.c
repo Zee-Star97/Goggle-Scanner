0933c569936f252a9d8761ef40a40ba54057d622
Zeeshan <zeeshanagario97@gmail.com>
2026-07-18 04:21:42 +0100

    + comment

--- sample_code/strcpy.c
+++ sample_code/strcpy.c
@@ -3,7 +3,7 @@
 
 int main() {
     char buffer[10];
-    strcpy(buffer, "This is a very long string");
+    strcpy(buffer, "This is a very long string"); // Contains a string longer than the buffer size
     printf("%s\n", buffer);
     return 0;
 }
